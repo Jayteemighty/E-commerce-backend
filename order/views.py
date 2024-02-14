96414eb4ef3d99db5c7bb48cec97521176a6bf02
Adesina Joshua <tolujosh1@gmail.com>
2024-02-14 16:14:49 +0100

    feature:pagination added

--- order/views.py
+++ order/views.py
@@ -27,7 +27,7 @@ def checkout(request):
             charge = stripe.Charge.create(
                 amount=int(paid_amount * 100),
                 currency='USD',
-                description='Charge from Djackets',
+                description='Charge',
                 source=serializer.validated_data['stripe_token']
             )
 
