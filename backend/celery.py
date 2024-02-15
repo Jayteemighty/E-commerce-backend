718a5948c89422d8078b72c5e6754d68e0fa49f2
Adesina Joshua <tolujosh1@gmail.com>
2024-02-15 22:22:11 +0100

    corrections for celery

--- backend/celery.py
+++ backend/celery.py
@@ -1,12 +1,12 @@
-from __future__ import absolute_import, unicode_literals
-import os
-from celery import Celery
-from django.conf import settings
+#from __future__ import absolute_import, unicode_literals
+#import os
+#from celery import Celery
+#from django.conf import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
-app = Celery("backend")
+#app = Celery("backend")
 
-app.config_from_object("django.conf:settings", namespace="CELERY")
+#app.config_from_object("django.conf:settings", namespace="CELERY")
 
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+#app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
